820a74d0ba9a15b928bc414305780ee33a547668
SoftOSDev <117074497+SoftOSDev@users.noreply.github.com>
2024-08-21 08:45:11 +0800

    Update table.js

--- table.js
+++ table.js
@@ -1,9 +1,9 @@
 //people:人员 money:数额 changes:变动 reason:原因
 //以数组的形式储存
-var people=new Array("Q2K_", "xiaoqian_233", "NoCodeRockStar","FILLYLIN_light","Undefined（）","syx1233456");
-var money=new Array("114514", "10", "10","0","0","60");
+var people=new Array("Q2K_", "aaa111", "NoCodeRockStar","FILLYLIN_light","Undefined（）","syx1233456");
+var money=new Array("15R", "9R", "10","0","0","60");
 var changes=new Array("无", "无", "无","无","无","无");
-var reason=new Array("开服", "摸鱼，管理服务器", "设计世界纪录村交所","此前因考试暂未上线","因学业未上线","修建大量红石机器；研发；赞助500");
+var reason=new Array("开服,维护", "使用绿宝石兑换", "设计世界纪录村交所","此前因考试暂未上线","因学业未上线","修建大量红石机器；研发；赞助500");
 
 //运行
 var line;
